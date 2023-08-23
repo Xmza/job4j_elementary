d80462e30469b4acce9319585baa648068cd12ac
xmz <pochtanomer3@bk.ru>
2023-08-23 20:21:54 +0300

    6.4.0. Слово начинается с ... [#41585 #438942]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,11 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startsWith(char[] word, char[] pref) {
-        for (int i = 1; i < word.length; i++) {
-            for (int j = 1; j < pref.length; j++) {
-                if (word[i] == pref[j]) {
-                    return true;
+        for (int i = 1; i < pref.length; i++) {
+                if (word[i] != pref[i]) {
+                    return false;
                 }
-            }
         }
-        return false;
+        return true;
     }
 }
