d295dbe173758e65398fa04b353aaa5204726a92
Xmza <mountain.xmz@gmail.com>
2023-09-09 20:23:30 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,46 +8,38 @@ import static org.assertj.core.api.Assertions.withPrecision;
 class PointTest {
 
     @Test
-    void when00to20then2() {
+    public void when00to20then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when30to20then2() {
+        Point a = new Point(3, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         double expected = 1;
-        int x1 = 3;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when00to21then2Dot23() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 1);
+        double out = a.distance(b);
         double expected = 2.23;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when20to20then0() {
+        Point a = new Point(2, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
         double expected = 0;
-        int x1 = 2;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
 }
